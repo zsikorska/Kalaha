c6d34b46ccdbe1b2c2de8cc446832967fc0a548c
zsikorska <sikorska.zuzia@gmail.com>
2022-06-17 16:37:35 +0200

    create base, start and sign_in  template, add template inheritance

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('start.html')
+
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
 
 
 if __name__ == '__main__':
