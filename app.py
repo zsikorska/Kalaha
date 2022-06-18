6e3d23f991ee6be868dceb169f1a582a4e780804
zsikorska <sikorska.zuzia@gmail.com>
2022-06-18 02:17:26 +0200

    some changes to templates and app, add template for creating account, game

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('start.html')
+    if request.method == 'GET':
+        return render_template('start.html')
+    else:
+        return redirect('/sign_in')
 
 
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html')
+
+
+@app.route('/create_account')
+def create_account():
+    return render_template('create_account.html')
+
+
+@app.route('/game')
+def play():
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
